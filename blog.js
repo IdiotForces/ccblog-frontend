e7056e51ec2d9f4b51e7cb6c5739d583bce56936
secondwtq <lovejay-lovemusic@outlook.com>
2015-04-09 03:56:10 +0800

    Initial Commit.

---
+++ blog.js
@@ -0,0 +1,70 @@
+var ISDCBlogDApp = angular.module('isdcng.blogdemo', [ 'isdcng.blogdemo.devtest' ]);
+
+ISDCBlogDApp.provider('myBlog', function () {
+	this.display_name = 'ISDCBlog';
+
+	this.set_display_name = function (value) {
+		this.display_name = value; };
+
+	this.$get = function () {
+		var display_name = this.display_name;
+		return {
+			get_display_name: function () { return display_name; }
+		};
+	};
+});
+
+ISDCBlogDApp.service('mainArticleListPages', function (articlesService) {
+
+	var item_per_page = 5;
+
+	var set_item_per_page = function (value) { item_per_page = value };
+
+	var get_total_pages = function () {
+		return Math.ceil(articlesService.total_len() / item_per_page); }
+
+	return {
+		set_item_per_page: set_item_per_page,
+		get_total_pages: get_total_pages
+	};
+});
+
+ISDCBlogDApp.service('utilService', function () {
+	var rep_range = function (n) { return new Array(n); };
+
+	return { rep_range: rep_range };
+});
+
+ISDCBlogDApp.config(function (myBlogProvider) {
+	myBlogProvider.set_display_name("示例博客"); });
+
+ISDCBlogDApp.controller('BlogController', function ($scope, myBlog) {
+	$scope.myBlog = myBlog;
+});
+
+ISDCBlogDApp.controller('MainArticleListController', function ($scope, $rootScope, myBlog, articlesService, usersService, utilService, mainArticleListPages) {
+	$scope.articlesService = articlesService;
+	$scope.usersService = usersService;
+	$scope.utilService = utilService;
+	$scope.mainArticleListPages = mainArticleListPages;
+
+	$scope.current_page = 1;
+
+	$scope.get_current_page = function () {
+		return $scope.current_page; }
+
+	$scope.prev_page = function () {
+		if ($scope.current_page != 1)
+			$scope.nav_page($scope.current_page - 1); }
+
+	$scope.next_page = function () {
+		if($scope.current_page != mainArticleListPages.get_total_pages())
+			$scope.nav_page($scope.current_page + 1); }
+
+	$scope.nav_page = function (pn) {
+		$scope.current_page = pn; }
+
+	$scope.article_list_page = function (pn) {
+		return articlesService.article_list((pn-1)*5, pn*5); }
+
+});
---
+++ blog_demo.js
@@ -0,0 +1,107 @@
+ISDCBlogDemoModule = angular.module('isdcng.blogdemo.devtest', [ ]);
+
+var users = {
+	0: {
+		user_id: 0,
+		username: 'Windas',
+		user_url: 'http://www.scuisdc.com',
+		user_avatar: './assets/placeholder.png'
+	},
+
+	1: {
+		user_id: 1,
+		username: 'Dfb',
+		user_url: 'http://www.scuisdc.com',
+		user_avatar: './assets/placeholder.png'
+	}
+};
+
+var articles = [
+	{
+		author_id: 0,
+		title: '苏共亡党亡国二十年祭', title_secondary: '苏共为何亡党亡国，以及我们从中得到的启示。',
+		summary: 'The early chapters are organized in order of conceptual difficulty, starting',
+		date: '96-12-08',
+		category: ['毛概']
+	},
+
+	{
+		author_id: 1,
+		title: '人脑计算机展望', title_secondary: '如何取得人脑的 root 权限？基础带你一探究竟。',
+		summary: 'The early chapters are organized in order of conceptual difficulty, starting',
+		date: '96-12-08',
+		category: ['基础', '人脑计算机']
+	},
+
+	{
+		author_id: 1,
+		title: '人脑计算机展望2', title_secondary: '如何取得人脑的 root 权限？基础带你一探究竟。',
+		summary: 'The early chapters are organized in order of conceptual difficulty, starting',
+		date: '96-12-08',
+		category: ['基础', '人脑计算机']
+	},
+
+	{
+		author_id: 1,
+		title: '人脑计算机展望3', title_secondary: '如何取得人脑的 root 权限？基础带你一探究竟。',
+		summary: 'The early chapters are organized in order of conceptual difficulty, starting',
+		date: '96-12-08',
+		category: ['基础', '人脑计算机']
+	},
+
+	{
+		author_id: 1,
+		title: '人脑计算机展望4', title_secondary: '如何取得人脑的 root 权限？基础带你一探究竟。',
+		summary: 'The early chapters are organized in order of conceptual difficulty, starting',
+		date: '96-12-08',
+		category: ['基础', '人脑计算机']
+	},
+
+	{
+		author_id: 1,
+		title: '人脑计算机展望5', title_secondary: '如何取得人脑的 root 权限？基础带你一探究竟。',
+		summary: 'The early chapters are organized in order of conceptual difficulty, starting',
+		date: '96-12-08',
+		category: ['基础', '人脑计算机']
+	},
+
+	{
+		author_id: 1,
+		title: '人脑计算机展望6', title_secondary: '如何取得人脑的 root 权限？基础带你一探究竟。',
+		summary: 'The early chapters are organized in order of conceptual difficulty, starting',
+		date: '96-12-08',
+		category: ['基础', '人脑计算机']
+	},
+];
+
+ISDCBlogDemoModule.service('articlesService', function () {
+
+	var total_len = function () { return articles.length; };
+	var article_list = function (start, end) {
+		if (start == null || start < 0)
+			start = 0;
+		if (end == null || end >= total_len())
+			end = total_len();
+		var ret = new Array();
+
+		for (var i = start; i < end; i++)
+			ret.push(articles[i]);
+		return ret;
+	};
+
+	return {
+		total_len: total_len,
+		article_list: article_list
+	};
+});
+
+ISDCBlogDemoModule.service('usersService', function () {
+
+	var get_userinfo = function (id) {
+		return users[id];
+	}
+
+	return {
+		get_userinfo: get_userinfo
+	};
+});
